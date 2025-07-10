9f23a665239ad5692576bcfbbc4686b9be072fdc
MichalSoowaa <michal.soowaa222@gmail.com>
2025-07-11 00:02:14 +0200

    fixes

--- src/main/java/dev/vetapp/database/DatabaseConnector.java
+++ src/main/java/dev/vetapp/database/DatabaseConnector.java
@@ -38,6 +38,7 @@ public class DatabaseConnector {
         }
         catch (SQLException e){
             logger.warn(e.getMessage());
+            e.printStackTrace();
         }
     }
 
@@ -64,8 +65,6 @@ public class DatabaseConnector {
         try{
             TableUtils.createTableIfNotExists(connectionSource, AppointmentEntity.class);
             TableUtils.createTableIfNotExists(connectionSource, ClientEntity.class);
-            TableUtils.createTableIfNotExists(connectionSource, InventoryEntity.class);
-            TableUtils.createTableIfNotExists(connectionSource, PaymentEntity.class);
             TableUtils.createTableIfNotExists(connectionSource, AnimalEntity.class);
             TableUtils.createTableIfNotExists(connectionSource, AnimalTypeEntity.class);
         }
@@ -73,69 +72,12 @@ public class DatabaseConnector {
             logger.warn("FAIL AT CREATING TABLES" + e.getMessage());
             e.printStackTrace();
         }
-
-//        try (Connection conn = DriverManager.getConnection(connectionString, user, password);
-//             Statement stmt = conn.createStatement()) {
-//
-//            String sql = "CREATE TABLE IF NOT EXISTS PetTypes ("
-//                    + "id INT PRIMARY KEY AUTO_INCREMENT, "
-//                    + "species VARCHAR(255) NOT NULL"
-//                    + "breed VARCHAR(255) NOT NULL"
-//                    + ");";
-//
-//            stmt.executeUpdate(sql);
-//            System.out.println("Table created successfully!");
-//
-//            sql = "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Mieszany')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Labrador Retriever')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Owczarek niemiecki')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Golden Retriever')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Chihuahua')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Buldog francuski')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Jamnik')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Border Collie')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Pies', 'Yorkshire Terrier')," +
-//                    "" +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Kot', 'Mieszany')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Kot', 'Maine Coon')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Kot', 'Syjamski')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Kot', 'Ragdoll')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Kot', 'Perski')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Kot', 'Sfinks')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Kot', 'Bengalski')," +
-//                    "" +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Królik', 'Mieszany')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Królik', 'Holenderski')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Królik', 'Nowozelandzki')," +
-//                    "" +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Chomik', 'Mieszany')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Chomik', 'Syryjski')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Chomik', 'Roborowski')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Chomik', 'Dżungalski')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Chomik', 'Chiński')," +
-//                    "" +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Świnka morska', 'Peruwiańska')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Świnka morska', 'Rozetka')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Świnka morska', 'Skinny')," +
-//                    "" +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Ptak', 'Kanarek')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Ptak', 'Ara')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Ptak', 'Papużka falista')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Ptak', 'Nimfa')," +
-//                    "INSERT INTO PetTypes(species, breed) VALUES('Ptak', 'Żako'),";
-//        }
-//        catch (SQLException e) {
-//            logger.warn(e.getMessage());
-//            //e.printStackTrace();
-//        }
     }
 
     private static void dropTables(){
         try{
             TableUtils.dropTable(connectionSource, AppointmentEntity.class, true);
             TableUtils.dropTable(connectionSource, ClientEntity.class, true);
-            TableUtils.dropTable(connectionSource, InventoryEntity.class, true);
-            TableUtils.dropTable(connectionSource, PaymentEntity.class, true);
             TableUtils.dropTable(connectionSource, AnimalEntity.class, true);
             TableUtils.dropTable(connectionSource, AnimalTypeEntity.class, true);
         }
